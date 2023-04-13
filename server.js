deb544ec2737ddcb4599b78e182587ce84db7fd2
AshleySSH <112930538+AshleySSH@users.noreply.github.com>
2023-04-13 10:29:42 +0600

    Add files via upload

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const mongoose = require('mongoose')
+const path = require('path')
+const exphbs = require('express-handlebars')
+const todoRoutes = require('./routes/todos')
+
+const PORT = process.env.PORT || 3000;
+const app = express()
+
+const hbs = exphbs.create({
+    defaultLayout: 'main',
+    extname: 'hbs'
+})
+
+app.engine('hbs', hbs.engine)
+app.set('view engine', 'hbs')
+app.set('views', 'views')
+
+app.use(express.urlencoded({ extended: true }))
+app.use('/public', express.static(path.join(__dirname, 'public')))
+
+app.use(todoRoutes)
+
+
+async function start() {
+    try {
+        await mongoose.connect('mongodb+srv://somasamiev:jXFbMP159JuvwKlx@nodetodo.dpzsykq.mongodb.net/todo', {
+
+
+
+
+        })
+        app.listen((PORT), () => {
+            console.log('nikita uebok')
+        })
+    } catch (e) {
+        return console.log(e)
+    }
+}
+
+start()
